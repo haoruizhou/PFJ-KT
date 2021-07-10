b6bc50eb61b70b5eaed9e43b2460c04edd4e2ba5
Hally Zhou <haorui2002@gmail.com>
2021-07-10 15:12:40 -0700

    finishing export feature

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ import pandas as pd
 from datetime import datetime
 import pytz
 
-
 DEFAULT_CURRENCY = 'USD'
 DEFAULT_UNIT = 'Gallons'
 DEFAULT_FUEL = 'Diesel'
+
 
 # input format (PFJ): Card #,Card Description,Trx Date,Transaction #,Store Location,Product,Driver ID,Odometer,
 # Trailer,Trip,Vehicle ID,Quantity,Fuel,Merchandise,Detail Total,Invoice Amt
 # output format (Keep Truckin): Date', ',Time (UTC)', ',Jurisdiction', ',Driver,Vehicle', ',Fuel Type', ',Gallons/Liters', ',Volume', ',
 # USD/CAD', ',Total Cost', ',Vendor Name', ', Location,Miles/Kilometers,Odometer,Reference #,Notes
+
+
+def merge():
+    print()
+    # merge all excel under one directory, output as "input.csv"
+    # for use when cannot export as quarters
 
 
 def import_csv():
@@ -40,8 +46,7 @@ def split_date_time(date):
     return date, time
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+def fuel_tax():
     data_read = import_csv()
     print(data_read.to_string())
     # print initial data
@@ -58,7 +63,10 @@ if __name__ == '__main__':
     print(data_diesel.to_string())
     # print corrected data
 
-    data_export = pd.DataFrame(columns=['Date', 'Time(UTC)', 'Jurisdiction', 'Driver', 'Vehicle', 'Fuel Type', 'Gallons/Liters', 'Volume', 'USD/CAD', 'Total Cost', 'Vendor Name', 'Location', 'Miles/Kilometers', 'Odometer', 'Reference #', 'Notes'])
+    data_export = pd.DataFrame(
+        columns=['Date', 'Time(UTC)', 'Jurisdiction', 'Driver', 'Vehicle', 'Fuel Type', 'Gallons/Liters', 'Volume',
+                 'USD/CAD', 'Total Cost', 'Vendor Name', 'Location', 'Miles/Kilometers', 'Odometer', 'Reference #',
+                 'Notes'])
 
     data_diesel_date = data_diesel["Trx Date"]
     data_diesel_store = data_diesel["Store Number"]
@@ -85,8 +93,38 @@ if __name__ == '__main__':
 
     # TODO
     print(data_diesel_converted_time.to_string())
-    data_export = pd.concat([data_diesel_converted_time, data_diesel_state, data_diesel_vehicle, data_diesel_vehicle])
+    # data_export = pd.concat([data_diesel_converted_time, data_diesel_state, data_diesel_vehicle, data_diesel_vehicle])
+
+    # dump data into export csv
+    data_export['Date'] = data_diesel_converted_time['Date']
+    data_export['Time(UTC)'] = data_diesel_converted_time['Time(UTC)']
+    data_export['Jurisdiction'] = data_diesel_state
+    data_export['Fuel Type'] = DEFAULT_FUEL
+    data_export['Gallons/Liters'] = DEFAULT_UNIT
+    data_export['USD/CAD'] = DEFAULT_CURRENCY
+    for index, value in data_diesel_vehicle.items():
+        try:
+            value = int(value)
+        except:
+            value = 0
+        data_diesel_vehicle[index] = value
+    # TODO: fix error
+    print(data_diesel_vehicle.to_string())
+    data_export['Vehicle'] = data_diesel_vehicle
+    data_export['Volume'] = data_diesel_volume
+    # TODO: remove $ sign
+    data_export['Total Cost'] = data_diesel_invoice_amount
+    for index, value in data_diesel_store.items():
+        value = "PFJ #" + str(value)
+        data_diesel_store[index] = value
+    # TODO: fix error
+    print(data_diesel_store.to_string())
+    data_export['Vendor Name'] = data_diesel_store
+
     print(data_export.to_string())
+    return data_export.to_csv('output.csv', sep=',', index=False)
 
-
-    #a = df1.join(numbers)
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print()
+    fuel_tax()
